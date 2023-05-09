6c20fe989cf51f70dc853bf7c897957bc5de5185
rubensalvarenga <rubens_alvarenga@hotmail.com>
2023-05-08 23:23:53 -0300

    Atualização

---
+++ Fundamentos.Metodo/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Fundamentos.Metodo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MeuMetodo();
+            Console.BackgroundColor = ConsoleColor.Red;   
+            string nome = RetornaNome("Rubens", "Alvarenga", 51);
+            Console.WriteLine(nome);
+            Console.BackgroundColor = ConsoleColor.Black;
+        }
+
+        static void MeuMetodo()
+        {
+            Console.WriteLine("C# é legal !!!");
+
+            
+
+            Console.WriteLine("");
+            Console.WriteLine("");
+        }
+
+        static string RetornaNome ( string primeiro , string segundo, int idade = 21)
+        {
+            return (primeiro + " " + segundo+", idade : "+idade.ToString()+" anos");
+        }
+    }
+}
